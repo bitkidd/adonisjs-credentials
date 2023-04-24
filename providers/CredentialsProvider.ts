62d75b842d3c524b8ae070903a6fb05536d32535
Chirill Ceban <2136460+bitkidd@users.noreply.github.com>
2023-04-24 17:40:33 +0100

    chore: expose provider to app

--- providers/CredentialsProvider.ts
+++ providers/CredentialsProvider.ts
@@ -8,7 +8,14 @@
  */
 
 import { ApplicationContract } from '@ioc:Adonis/Core/Application'
+import { Credentials } from '../src/Credentials'
 
-export default class MeiliSearchProvider {
+export default class CredentialsProvider {
   constructor(protected app: ApplicationContract) {}
+
+  public async register() {
+    this.app.container.singleton('Adonis/Addons/Credentials', () => {
+      return new Credentials()
+    })
+  }
 }
