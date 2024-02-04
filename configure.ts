c924c00e4a5350f6b92d350e7631c186013a9e25
Chirill Ceban <2136460+bitkidd@users.noreply.github.com>
2024-02-04 15:29:14 +0000

    feat: update gitignore on configure

--- configure.ts
+++ configure.ts
@@ -14,8 +14,10 @@
 
 import ConfigureCommand from '@adonisjs/core/commands/configure'
 import { stubsRoot } from './stubs/main.js'
+import { FileSystem } from './src/filesystem.js'
 
 export async function configure(command: ConfigureCommand) {
+  const filesystem = new FileSystem()
   const codemods = await command.createCodemods()
 
   await codemods.updateRcFile((rcFile) => {
@@ -23,4 +25,24 @@ export async function configure(command: ConfigureCommand) {
   })
 
   await codemods.makeUsingStub(stubsRoot, 'start.stub', {})
+
+  try {
+    const oldGitIgnoreFile = await filesystem.read({ path: `${command.app.makePath()}/.gitignore` })
+    const keyRuleExists = oldGitIgnoreFile.includes('*.key')
+
+    if (keyRuleExists) {
+      command.logger.action('update .gitignore file').skipped()
+    } else {
+      const newGitIgnoreFile = `${oldGitIgnoreFile}\n*.key\n`
+
+      await filesystem.write({
+        path: `${command.app.makePath()}/.gitignore`,
+        data: newGitIgnoreFile,
+      })
+
+      command.logger.action('update .gitignore file').succeeded()
+    }
+  } catch (error) {
+    command.logger.action('update .gitignore file').failed(error)
+  }
 }
